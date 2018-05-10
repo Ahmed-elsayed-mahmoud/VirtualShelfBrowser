9c69899b368cc5ae891879682da5407d3de345c9
Hazem Ashmawy <hazem.s.ashmawy@gmail.com>
2018-05-10 13:39:15 +0200

    Modal in separate component

--- app/components/BookInfo.js
+++ app/components/BookInfo.js
@@ -6,31 +6,22 @@ class BookInfo extends Component {
     constructor(props, context) {
         super(props, context);
 
-        this.handleShow = this.handleShow.bind(this);
         this.handleClose = this.handleClose.bind(this);
         this.controller = Controller.getInstance();
 
         this.state = {
-            show: false,
             reviews: ''
         };
     }
 
     handleClose() {
-        this.setState({show: false});
+        this.props.hide();
     }
 
-    /*getReviewsView(isbn) {
-        this.BASE_URL = 'https://www.goodreads.com/book/isbn/'
-        this.KEY = 'NwNSlVu7xFWbuomMXJhrzA';
-        return fetch(`${this.BASE_URL}${isbn}?key=${this.KEY}&format=json`, { method: 'GET' })
-            .then(response => {
-                return response.json();
-            });
-    }*/
-
-    handleShow() {
-        this.setState({show: true});
+    componentWillReceiveProps(nextProps) {
+        if (this.props.book.ISBN === nextProps.book.ISBN)
+            return;
+        this.setState({ reviews: '' });
         this.controller.selectBook(this.props.book.ISBN)
             .then(json => {
                 this.setState({reviews: json["reviews_widget"]});
@@ -42,87 +33,61 @@ class BookInfo extends Component {
 
     render() {
         let alternate = 'http://www.tpsudan.gov.sd/resources/files/images/placeholder.png';
-
         let book = this.props.book;
-        /*let book = {
-            title: 'the title',
-            ISBN: '123asda123123',
-            imageUrl: null,
-            description: 'aasdas;ldkas;lkdl;asd\nklaldsadklasdjklasdaslkdklasjdklasjkldjklasdlk\nkladklas',
-            rate: 0.0,
-            publisherName: 'Hamada El LOL',
-            publicationDate: '12/3/2019',
-            numberOfPages: 0,
-            readUrl: '',
-            authors: ['Dr. Zizo', 'Prof. Adel'],
-            storesLocations: ['ELPOP bookstore'],
-            reviews: []
-        };*/
 
         return (
-            <div className="book-modal">
-                <a onClick={this.handleShow}>
-                    <img src={book.imageUrl !== '' ? book.imageUrl : alternate} className="book-img" alt="book"/>
-                    <div className="book-text">{book.title}</div>
-                </a>
-
-                {/*<Button className="book-modal-btn" bsStyle="info" bsSize="large" onClick={this.handleShow}>
-                    Book Info
-                </Button>*/}
-
-                <Modal show={this.state.show} onHide={this.handleClose} keyboard>
-                    <Modal.Header closeButton>
-                        <a href={book.readUrl} target="_blank">
-                            <Modal.Title>{book.title}</Modal.Title>
-                        </a>
-                    </Modal.Header>
-                    <Modal.Body>
-                        <Row>
-                            <Col xs={12} md={5}>
-                                <a href={book.readUrl} target="_blank">
-                                    <img src={book.imageUrl ? book.imageUrl : alternate} className="img-responsive"
-                                         alt="book cover"/>
-                                </a>
-                                <div className="panel panel-warning">
-                                    <div className="panel-heading">Rate: {book.rate}</div>
-                                </div>
-                                <div className="panel panel-info">
-                                    <div className="panel-heading">ISBN</div>
-                                    <div className="panel-body">{book.ISBN}</div>
-                                </div>
-                                <div className="panel panel-info">
-                                    <div className="panel-heading">Publication Date</div>
-                                    <div className="panel-body">{book.publicationDate}</div>
-                                </div>
-                                <div className="panel panel-info">
-                                    <div className="panel-heading">Published by</div>
-                                    <div className="panel-body">{book.publisherName}</div>
-                                </div>
-                            </Col>
-                            <Col xs={12} md={7}>
-                                <div className="panel panel-info">
-                                    <div className="panel-heading">Authors</div>
-                                    <div
-                                        className="panel-body">{(book.authors !== undefined) ? book.authors.join(", ") : ""}</div>
-                                </div>
-                                <div className="panel panel-info">
-                                    <div className="panel-heading">Description</div>
-                                    <div className="panel-body">{book.description}</div>
-                                </div>
-                            </Col>
-                        </Row>
-                        <Row>
-                            <Col>
-                                <div className="container" dangerouslySetInnerHTML={{__html: this.state.reviews}}>
-                                </div>
-                            </Col>
-                        </Row>
-                    </Modal.Body>
-                    <Modal.Footer>
-                        <Button onClick={this.handleClose}>Close</Button>
-                    </Modal.Footer>
-                </Modal>
-            </div>
+            <Modal show={this.props.show} onHide={this.handleClose} keyboard>
+                <Modal.Header closeButton>
+                    <a href={book.readUrl} target="_blank">
+                        <Modal.Title>{book.title}</Modal.Title>
+                    </a>
+                </Modal.Header>
+                <Modal.Body>
+                    <Row>
+                        <Col xs={12} md={5}>
+                            <a href={book.readUrl} target="_blank">
+                                <img src={book.imageUrl ? book.imageUrl : alternate} className="img-responsive"
+                                        alt="book cover"/>
+                            </a>
+                            <div className="panel panel-warning">
+                                <div className="panel-heading">Rate: {book.rate}</div>
+                            </div>
+                            <div className="panel panel-info">
+                                <div className="panel-heading">ISBN</div>
+                                <div className="panel-body">{book.ISBN}</div>
+                            </div>
+                            <div className="panel panel-info">
+                                <div className="panel-heading">Publication Date</div>
+                                <div className="panel-body">{book.publicationDate}</div>
+                            </div>
+                            <div className="panel panel-info">
+                                <div className="panel-heading">Published by</div>
+                                <div className="panel-body">{book.publisherName}</div>
+                            </div>
+                        </Col>
+                        <Col xs={12} md={7}>
+                            <div className="panel panel-info">
+                                <div className="panel-heading">Authors</div>
+                                <div
+                                    className="panel-body">{(book.authors !== undefined) ? book.authors.join(", ") : ""}</div>
+                            </div>
+                            <div className="panel panel-info">
+                                <div className="panel-heading">Description</div>
+                                <div className="panel-body">{book.description}</div>
+                            </div>
+                        </Col>
+                    </Row>
+                    <Row>
+                        <Col>
+                            <div className="container" dangerouslySetInnerHTML={{__html: this.state.reviews}}>
+                            </div>
+                        </Col>
+                    </Row>
+                </Modal.Body>
+                <Modal.Footer>
+                    <Button onClick={this.handleClose}>Close</Button>
+                </Modal.Footer>
+            </Modal>
         );
     }
 }
--- app/components/Gallery.js
+++ app/components/Gallery.js
@@ -1,16 +1,53 @@
 import React, {Component} from 'react';
 import BookInfo from './BookInfo';
 
+class BookItem extends Component {
+    constructor(props, context) {
+        super(props, context);
+
+        this.state = {
+            show: false,
+        };
+    }
+
+    show() {
+        this.setState({show: true});
+    }
+
+    hide() {
+        this.setState({show: false});
+    }
+
+    render() {
+        let alternate = 'http://www.tpsudan.gov.sd/resources/files/images/placeholder.png';
+
+        let book = this.props.book;
+
+        return (
+            <div className="book-modal">
+                <a onClick={e => this.show()}>
+                    <img src={book.imageUrl !== '' ? book.imageUrl : alternate} className="book-img" alt="book"/>
+                    <div className="book-text">{book.title}</div>
+                </a>
+                <BookInfo
+                    book={this.props.book}
+                    show={this.state.show}
+                    hide={() => this.hide()}
+                />
+            </div>
+        );
+    }
+}
+
 class Gallery extends Component {
     render() {
-        //let alternate = 'http://www.isic.cz/wp-content/plugins/orchitech-dm/resources/alive-dm/img/empty-image.png';
         return (
             <div>
                 {
                     this.props.items.map((item, index) => {
                         return (
                             <div className="book" key={index}>
-                                <BookInfo book={item}/>
+                                <BookItem book={item}/>
                             </div>
                         )
                     })
